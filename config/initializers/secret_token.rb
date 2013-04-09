360157ca9f6143bdfbc38d96de988d06033a3f29
Noah Bouillon <noah.bouillon@gmail.com>
2013-04-09 17:28:07 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MwohDo::Application.config.secret_token = 'e7a7c7ea96fc305cb27e36ad8080a436b4a912ba924266c80a04478bcbe5dd43e8dadf78e743bcee80f25247d5ac8156fd7ff0a5f41905c775183c9f62a4c9cf'
