ab0f9b7b4191c8b7722cdeced46124cabec0194e
Noah Bouillon <noah.bouillon@gmail.com>
2013-04-09 23:41:54 -0400

    Card index page working

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -16,4 +16,25 @@ class CardsController < ApplicationController
   		render 'new'
   	end
   end
+
+  def edit
+  	@card = Card.find(params[:id])
+    if @card.update_attributes(params[:user])
+      flash[:success] = "Card updated"
+      redirect_to @card
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+  	@cards = Card.all
+  end
+
+  def destroy
+  	Card.find(params[:id]).destroy
+    flash[:success] = "Card destroyed."
+    redirect_to cards_url
+  end
+
 end
